1135639ad069a164bdcdd28019dee9e0bb011d7a
akemi <akemi.mullis@gmail.com>
2025-05-18 18:57:18 -0300

    Criação de endpoint usando HTML + testes

--- fast_api_zero/app.py
+++ fast_api_zero/app.py
@@ -1,9 +1,24 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_api_zero.schemas import Message
 
 app = FastAPI()
 
 
 # System Under Test - SUT
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá, mundo!'}
+    return {'message': 'Olá, Mundo!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_root_html():
+    return """
+    <html>
+        <head>
+            <h1> Olá, Mundo! </h1>
+        </head>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -22,5 +22,24 @@ def test_root_deve_retornar_ola_mundo():
     response = client.get('/')
 
     # assert
-    assert response.json == {'message': 'Olá, Mundo!'}
+    assert response.json() == {'message': 'Olá, Mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_root_deve_retornar_ola_mundo_html():
+    """
+    Esse teste tem etapas (AAA)
+    - A: Arrange - Arranjo
+    - A: Act - Executa a coisa (o SUT)
+    - A: Assert - Garanta que A é A
+    """
+
+    # arrange
+    client = TestClient(app)
+
+    # act
+    response = client.get('/html')
+
+    # assert
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá, Mundo! </h1>' in response.text
